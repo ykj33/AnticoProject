9c3feb32fc180efa886ff986ae521a8dbb0e0ab1
ykj <dnsrjswnd@gmail.com>
2020-07-26 23:00:19 +0900

    200726 윤건중 tbl_goods_option 오타 수정, list페이지 프로토타입 제작 및 관련 제반 controller 작성

--- src/main/java/kr/co/antico/service/BoardService.java
+++ src/main/java/kr/co/antico/service/BoardService.java
@@ -1,5 +1,11 @@
 package kr.co.antico.service;
 
+import java.util.List;
+
+import kr.co.domain.GoodsDTO;
+
 public interface BoardService {
+
+	List<GoodsDTO> list();
 
 }
--- src/main/java/kr/co/antico/service/BoardServiceImpl.java
+++ src/main/java/kr/co/antico/service/BoardServiceImpl.java
@@ -1,13 +1,21 @@
 package kr.co.antico.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.co.antico.persistence.BoardDAO;
+import kr.co.domain.GoodsDTO;
 
 @Service
 public class BoardServiceImpl implements BoardService {
 
 	@Autowired
 	private BoardDAO boardDao;
+
+	@Override
+	public List<GoodsDTO> list() {
+		return boardDao.list();
+	}
 }
