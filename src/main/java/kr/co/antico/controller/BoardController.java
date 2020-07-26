1bf3e124317a87cdb096964724b73cd38f9cedce
lovebox0310 <lovebox0310@gmail.com>
2020-07-27 00:14:27 +0900

    20200727 lovebox0310 boardcontroller 충돌해결.

--- src/main/java/kr/co/antico/controller/BoardController.java
+++ src/main/java/kr/co/antico/controller/BoardController.java
@@ -1,12 +1,16 @@
 package kr.co.antico.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import kr.co.antico.service.BoardService;
+import kr.co.domain.GoodsDTO;
 
 @Controller
 @RequestMapping("board")
@@ -14,14 +18,18 @@ public class BoardController {
 	
 	@Autowired
 	private BoardService boardService;
-	
-	@RequestMapping("/list")
-	public void list() {
-		
-	}
 
-	@RequestMapping(value = "/read{bno}", method = RequestMethod.GET)
-	public void read(@PathVariable("bno") int bno) {
+	
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public String list(Model model) {
+		List<GoodsDTO> list = boardService.list();
+		model.addAttribute("list", list);
+		return "board/list";
+	}
+	
+	// 상세 페이지
+	@RequestMapping(value = "/read", method = RequestMethod.GET)
+	public void read() {
 //		boardService.read(bno);
 	}
 }
